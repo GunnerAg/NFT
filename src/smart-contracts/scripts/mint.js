ab9d24feff571e1b0000138b1919ed29fbbead49
Gunner Andersen <gunner.andersen.gil@gmail.com>
2022-06-09 21:50:55 +0200

    feat:adding first commit

---
+++ src/smart-contracts/scripts/mint.js
@@ -0,0 +1,110 @@
+const contractJson = require("../build/Test.json");
+const dotenv = require("dotenv");
+// Set the NODE_ENV to 'development' by default
+process.env.NODE_ENV = process.env.NODE_ENV || "development";
+
+if (process.env.NODE_ENV === "development") {
+  const envFound = dotenv.config();
+  if (envFound.error) {
+    // This error should crash whole process
+    throw new Error("⚠️  Couldn't find .env file  ⚠️");
+  }
+}
+
+// Get web3
+const Web3 = require("web3");
+var web3 = new Web3(
+  new Web3.providers.HttpProvider(
+    "https://rinkeby.infura.io/v3/c91b3318090544299c446a05dd64bc94"
+  )
+);
+
+// get contract
+const networkId = Object.keys(contractJson.networks)[0];
+const contract = new web3.eth.Contract(
+  contractJson.abi,
+  contractJson.networks[networkId].address
+);
+
+// Accounts to mint to.
+const accounts = [
+  "0xC967FDc1af9E0E93FF252D86650FF9B820a8f290",
+  "0x2A6d06aaf96416b0F1aA82C637e3add7b60d3466",
+  "0xEf9FaE4Cb7408EecCEB44De52B80B9D35971f6C6",
+];
+
+// Accounts that own a token.
+const hasToken = [];
+
+const run = async () => {
+  await contract
+    .getPastEvents("Transfer", {
+      fromBlock: 0,
+      toBlock: "latest",
+    })
+    .then(function (events) {
+      events.forEach((e) => {
+        const recipient = e.returnValues.to;
+        console.log("This has token", recipient);
+        !hasToken.includes(recipient) && hasToken.push(recipient);
+      });
+    });
+
+  const validAddresses = accounts.filter((acc) => {
+    return !hasToken.includes(acc);
+  });
+
+  console.log('original accounts', accounts);
+  console.log("hasToken", hasToken);
+  console.log("validAddresses", validAddresses);
+  // Sleep()
+  if(validAddresses.length===0){
+    console.log('All accounts have tokens');
+  }
+  await new Promise((r) => setTimeout(r, 1000 * 5));
+
+  // mint tokens
+  for (let i = 0; i < validAddresses.length; i++) {
+    // Sleep()
+    console.log("Minting to: " + validAddresses[i]);
+    //await new Promise((r) => setTimeout(r, 1000 * 10));
+    const price = await web3.eth.getGasPrice();
+    const tx = {
+      from: process.env.PUBLIC_KEY,
+      to: contractJson.networks[networkId].address,
+      gas: 5500000,
+      gasPrice: price,
+      data: contract.methods.safeMint(validAddresses[i]).encodeABI(),
+    };
+    // Sleep()
+    // await new Promise((r) => setTimeout(r, 1000 * 60));
+    await new Promise(async (resolve, reject) => {
+      await web3.eth.accounts
+        .signTransaction(tx, process.env.PRIVATE_KEY)
+        .then((signedTx) => {
+          const sentTx = web3.eth.sendSignedTransaction(
+            signedTx.raw || signedTx.rawTransaction
+          );
+          sentTx.on("receipt", (receipt) => {
+            console.table(
+              "Transaction receipt: " + JSON.stringify(receipt, null, 4)
+            );
+            resolve();
+          });
+          sentTx.on("error", (err) => {
+            console.log("Transaction error: " + err);
+          });
+        })
+        .catch((err) => {
+          console.log("Signing error: " + err);
+          reject();
+        });
+    });
+    console.log("Minted to: " + validAddresses[i]);
+    await new Promise((r) => setTimeout(r, 1000 * 2));
+    console.log("There are: " + validAddresses.length-i + "itereations left");
+  }
+  console.log("Finished minting");
+};
+
+run();
