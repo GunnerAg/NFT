aa2bc31999dbdc4d4099e8bc50dd9778bd38e87e
Gunner Andersen <gunner.andersen.gil@gmail.com>
2022-06-13 21:12:35 +0200

    add node scripts to package.json

--- src/smart-contracts/contracts/Test.sol
+++ src/smart-contracts/contracts/Test.sol
@@ -5,9 +5,14 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+
+
 
 contract Test is ERC721, ERC721Enumerable, Ownable {
     using Counters for Counters.Counter;
+    using Strings for uint256;
+    string private baseExtension = '.json';
 
     Counters.Counter private _tokenIdCounter;
 
@@ -23,12 +28,39 @@ contract Test is ERC721, ERC721Enumerable, Ownable {
         _safeMint(to, tokenId);
     }
 
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        virtual
+        override
+        returns (string memory)
+    {
+        require(
+            _exists(tokenId),
+            "ERC721Metadata: URI query for nonexistent token"
+        );
+
+        string memory currentBaseURI = _baseURI();
+        return
+            bytes(currentBaseURI).length > 0
+                ? string(
+                    abi.encodePacked(
+                        currentBaseURI,
+                        tokenId.toString(),
+                        baseExtension
+                    )
+                )
+                : "";
+    }
+
     // The following functions are overrides required by Solidity.
 
-    function _beforeTokenTransfer(address from, address to, uint256 tokenId)
-        internal
-        override(ERC721, ERC721Enumerable)
-    {
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 tokenId
+    ) internal override(ERC721, ERC721Enumerable) {
         super._beforeTokenTransfer(from, to, tokenId);
     }
 
